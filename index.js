7a60eaf901f051839aeeb8bf89dfd3cedbd6e4bd
Thallis <thallisgabriel66@gmail.com>
2026-05-12 23:31:54 -0300

    G5 G6 - ForEach, If e Else

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function (field, index) {
+
+    if (field.name == "gender") {
+
+        if (field.checked === true) {
+
+            console.log("SIM", field);
+
+        }
+
+    } else {
+
+        console.log("NÂO");
+
+    }
+
+    //console.log(field.id, field.name, field.value, field.checked, index);
+
+});
