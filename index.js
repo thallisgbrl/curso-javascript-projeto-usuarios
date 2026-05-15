4f4108b6193b6662869829c1fd7fea45cedf6372
Thallis <thallisgabriel66@gmail.com>
2026-05-14 23:48:20 -0300

    G10 - Programação Orientada a Objetos

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ var user = {};
 
 function addLine(dataUser) {
 
-    var tr = document.createElement("tr");
-
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
         <tr>
                     <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
                     <td>${dataUser.name}</td>
-                    <td>fulano@hcode.com.br</td>
-                    <td>Sim</td>
-                    <td>02/04/2018</td>
+                    <td>${dataUser.email}</td>
+                    <td>${dataUser.admin}</td>
+                    <td>${dataUser.birth}</td>
                     <td>
                       <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                       <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
@@ -19,8 +17,7 @@ function addLine(dataUser) {
                   </tr>
         `;
 
-    document.getElementById("table-users").appendChild(tr);
-
+        
 }
 
 document.getElementById("form-user-create").addEventListener("submit", function (event) {
